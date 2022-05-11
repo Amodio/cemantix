e08b44033fc2333c66ff7705e210281a6853b407
Jacques Boscq <jacques@boscq.fr>
2022-05-11 12:58:56 +0200

    Add golang program to test words validity

---
+++ goTestWords/main.go
@@ -0,0 +1,60 @@
+// Test words from 'wordlist.txt', outputs valid ones into 'output.txt'
+package main
+
+import (
+    "bufio"
+    "log"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "os"
+)
+
+const unknownStr = "{\"error\":"
+const unknownErr = "{\"error\":\"Je ne connais pas "
+
+func sendWord(word string) string {
+    resp, err := http.PostForm("https://cemantix.herokuapp.com/score",
+                                url.Values{ "word": {word} })
+    if err != nil {
+        log.Fatal(word, err, resp)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(word, err, body)
+    }
+    return string(body)
+}
+
+func checkWords(pathname string) {
+    file, err := os.Open(pathname)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    outputFile, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        panic(err)
+    }
+    defer outputFile.Close()
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+	ret := sendWord(scanner.Text())
+        if len(ret) > len(unknownStr) && ret[0:len(unknownStr)] == unknownStr {
+            if len(ret)>len(unknownErr) && ret[0:len(unknownErr)]==unknownErr {
+	    } else {
+                log.Fatal(scanner.Text(), ret)
+            }
+	} else {
+            outputFile.WriteString(scanner.Text() + "\n")
+        }
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(scanner.Text(), err)
+    }
+}
+
+func main() {
+    checkWords("wordlist.txt")
+}
