e8cb5d9cf7316010e08d44389dca3d8ada9a2a9f
Jacques Boscq <jacques@boscq.fr>
2022-12-23 00:40:33 +0100

    remove old bash scripts

--- utils/1_extractWordlists.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in *.bin; do
-    name="${file%????}"
-    ./convertvec bin2txt "$name.bin" "$name.txt"
-    awk '{print $1}' "$name.txt" | awk NR\>1 > "$name.list"
-done
-wc -l *.list
--- utils/README.md
+++
@@ -1,4 +0,0 @@
-Bash scripts to extract wordlists from word2vec model binary files.
-
-Test the extracted words with the golang script then regenerate the binary
-models by running `99_trim_word2vec.sh`.
